9f06b662a5559e2c39ca596fe08f2693b7450e6d
ryokotokuhisa <rtokuhisa@aitech.ac.jp>
2024-09-17 17:16:48 +0900

    教員の個人ページへのリンク方法を変更（コンポーネント全体をリンク）

--- src/component/member/teacher/Teacher.jsx
+++ src/component/member/teacher/Teacher.jsx
@@ -3,9 +3,9 @@ import styles from './staticMain.module.css';
 
 export default function Teacher({ teacher }) {
   return (
-    <div className={styles.teacher}>
-      <div className={styles.profInfo}>
-        <a href={teacher.link} target="_blank" rel="noopener noreferrer"> {/* ここでリンクを設定 */}
+    <a href={teacher.link} target="_blank" rel="noopener noreferrer"> {/* ここでリンクを設定 */}
+      <div className={styles.teacher}>
+        <div className={styles.profInfo}>
           <div className={styles.profImage}>
             <Image
               src={teacher.image}
@@ -15,11 +15,9 @@ export default function Teacher({ teacher }) {
               style={{ objectFit: 'cover' }}
             />
           </div>
-        </a>
-        <a href={teacher.link} target="_blank" rel="noopener noreferrer"> {/* 名前部分もリンク化 */}
           <p>{teacher.description}</p>
-        </a>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
