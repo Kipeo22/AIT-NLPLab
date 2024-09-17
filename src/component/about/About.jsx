a4c9cac826364ac4da59e756fc7f1583db13536c
ryokotokuhisa <rtokuhisa@aitech.ac.jp>
2024-09-17 18:08:44 +0900

    ABOUTって表示するのだけやめる

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <section id='about' className={styles.section}>
       <div className={styles.about}>
-        <h2>About</h2>
+        {/* <h2>About</h2> */} 
         <p>
           <a href="https://www.ait.ac.jp/faculty/info-science/">愛知工業大学情報科学部情報科学科</a>自然言語処理研究室（徳久研究室）のホームページです。
         </p>
