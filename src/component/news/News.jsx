9f58211cc336e1cb8d61ff1fe645ba53a2bae5aa
ryokotokuhisa <rtokuhisa@aitech.ac.jp>
2024-09-17 13:05:37 +0900

    Newsのレイアウト変更

--- src/component/news/News.jsx
+++ src/component/news/News.jsx
@@ -2,16 +2,41 @@ import Image from 'next/image';
 import styles from './staticMain.module.css';
 
 export default function News() {
+
+  const newsData = [
+    {
+      image: '/news_img/P1047364.jpg',
+      date: '2024/09/17',
+      title: 'タイトル1',
+      description: '説明1を書く。1行から2行程度が望ましい。',
+    },
+    {
+      image: '/news_img/P1047365.jpg',
+      date: '2024/09/18',
+      title: 'タイトル2',
+      description: '説明2を書く。内容は簡潔に。',
+    },
+    {
+      image: '/news_img/P1047366.jpg',
+      date: '2024/09/19',
+      title: 'タイトル3',
+      description: '説明3を書く。詳細を伝える。',
+    },
+  ];
+
   return (
     <section id='news' className={styles.section}>
       <div className={styles.news}>
         <h2>News</h2>
         <div className={styles.NewsContainer}>
-          {[...Array(3)].map((_, index) => (
+          {newsData.map((newsItem, index) => (
             <div key={index} className={styles.NewsItem}>
-              <Image src='/news_img/P1047364.jpg' alt='sample news' width={340} height={230} />
-              <h3>タイトル</h3>
-              <p>せつめいを書く。１行から、２行程度がでかけるといい</p>
+              <Image src={newsItem.image} alt={`news image ${index + 1}`} width={340} height={230} />
+              <div className={styles.NewsText}>
+                <p>{newsItem.date}</p> {/* 各ニュースごとの日付 */}
+                <h3>{newsItem.title}</h3> {/* 各ニュースごとのタイトル */}
+                <p>{newsItem.description}</p> {/* 各ニュースごとの説明 */}
+              </div>
             </div>
           ))}
         </div>
