dbb2732b1614e0b432092751aa7c159174b22857
ryokotokuhisa <rtokuhisa@aitech.ac.jp>
2024-09-17 17:02:04 +0900

    住所修正

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -5,7 +5,7 @@ export default function Contact() {
     <section id='contact' className={styles.section}>
       <div className={styles.contact}>
         <h2>Contact</h2>
-        <p>住所：〒470-0392 豊田市八草町八千草1247 愛知工業大学情報科学部 14号館708号</p>
+        <p>住所：〒470-0392 豊田市八草町八千草1247 愛知工業大学情報科学部 14号館708号 徳久研究室</p>
         <p>電話：0565-48-8121(内線2416)</p>
       </div>
     </section>
