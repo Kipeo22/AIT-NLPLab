643c0448eb40ebfbc2d05fe924f65b46936bfa0b
ryokotokuhisa <rtokuhisa@aitech.ac.jp>
2024-09-17 17:59:45 +0900

    学生のdescriptionはとりあえずコメントアウト

--- src/component/member/Member.jsx
+++ src/component/member/Member.jsx
@@ -34,49 +34,49 @@ export default function Member() {
     {
       name: '市川淳貴',
       image: '/member_img/ichikawa.jpg',
-      description: '研究テーマ：',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
       link: 'https://example.com/url', // 個人ページURL
     },
     {
       name: '大塚晴貴',
       image: '/member_img/Otsuka.jpg',
-      description: '研究テーマ：',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
       link: 'https://example.com/url', // 個人ページURL
     },
 　　{
       name: '岡島史周',
       image: '/member_img/Okajima.jpg',
-      description: '研究テーマ：',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
       link: 'https://example.com/url', // 個人ページURL
     },
     {
       name: '柴田翔空',
       image: '/member_img/shibata.jpg',
-      description: '研究テーマ',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
       link: 'https://example.com/url', // 個人ページURL
     },
     {
       name: '清水綾太',
       image: '/member_img/shimizu.jpg',
-      description: 'がおーーーーーーーーーーーーーーー',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
       link: 'https://example.com/url', // 個人ページURL
     },
 　  {
       name: '長谷川騎平',
       image: '/member_img/kippei.png',
-      description: '研究テーマ：',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
       link: 'https://example.com/url', // 個人ページURL
     },
 　　{
       name: '肥田京佳',
       image: '/member_img/hida.jpg',
-      description: '研究テーマ：',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
       link: 'https://kyoka-hida.github.io', // 個人ページURL
     },
     {
       name: '増井辰真',
       image: '/member_img/masui.jpg',
-      description: '研究テーマ：',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
       link: 'https://example.com/url', // 個人ページURL
     },
 
