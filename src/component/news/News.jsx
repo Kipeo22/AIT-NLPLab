64ac250ae32c4d67cad20b11d80720e9831a3a53
ryokotokuhisa <rtokuhisa@aitech.ac.jp>
2024-09-17 13:31:26 +0900

    Newsにpresentations追加

--- src/component/news/News.jsx
+++ src/component/news/News.jsx
@@ -6,15 +6,24 @@ export default function News() {
   const newsData = [
     {
       image: '/news_img/P1047364.jpg',
-      date: '2024/09/17',
-      title: 'タイトル1',
-      description: '説明1を書く。1行から2行程度が望ましい。',
+      date: '2024/09/05',
+      title: '肥田京佳が「YANS2024奨励賞」受賞',
+      description: '肥田京佳が「YANS2024奨励賞」を受賞しました',
+      presentations: [
+        '「ふわふわ」「もったり」ってどう表現するの？ ーエージェントとの豊かなコミュニケーションの実現に向けてー・・・肥田 京佳, 市川 淳貴, 徳久 良子',
+      ]
     },
     {
       image: '/news_img/P1047364.jpg',
       date: '2024/09/18',
       title: 'タイトル2',
       description: '説明2を書く。内容は簡潔に。',
+    },
+    {
+      image: '/news_img/P1047364.jpg',
+      date: '2024/09/19',
+      title: 'タイトル3',
+      description: '説明3を書く。詳細を伝える。',
     },
     {
       image: '/news_img/P1047364.jpg',
@@ -36,6 +45,23 @@ export default function News() {
                 <p>{newsItem.date}</p> {/* 各ニュースごとの日付 */}
                 <h3>{newsItem.title}</h3> {/* 各ニュースごとのタイトル */}
                 <p>{newsItem.description}</p> {/* 各ニュースごとの説明 */}
+                <p>{news.description}</p>
+                {/* presentationsがある場合にリストを表示 */}
+                {news.presentations && (
+                  <ul className={styles.PresentationList}>
+                    {news.presentations.map((presentation, i) => {
+                      const [title, author] = presentation.split('・・・'); // タイトルと著者を分割
+                      return (
+                        <li key={i}>
+                          <strong>{title}</strong> {/* タイトル部分を太字に */}
+                          <ul className={styles.AuthorList}> {/* 著者部分を別のリストに */}
+                            <li>{author}</li>
+                          </ul>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                )}
               </div>
             </div>
           ))}
