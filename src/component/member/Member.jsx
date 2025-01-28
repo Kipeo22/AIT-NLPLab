418804454406d928700009acb03409489f3e2002
ryokotokuhisa <rtokuhisa@aitech.ac.jp>
2025-01-28 12:10:15 +0900

    余白の調整

--- src/component/member/Member.jsx
+++ src/component/member/Member.jsx
@@ -13,7 +13,7 @@ export default function Member() {
               target="_blank" 
               rel="noopener noreferrer"
             >徳久良子(Ryoko TOKUHISA) </a></strong>
-            <p>rtokuhisa at aitech.ac.jp</p>
+            <p>メール：rtokuhisa at aitech.ac.jp</p>
           <ul>
             <li>研究活動
               <ul>
