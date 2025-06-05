0107d2fad7cab0f532c8522f95dd2b38152a4e99
ryokotokuhisa <rtokuhisa@aitech.ac.jp>
2025-06-05 11:25:59 +0900

    B3追加

--- src/component/member/Member.jsx
+++ src/component/member/Member.jsx
@@ -97,44 +97,68 @@ export default function Member() {
       link: 'https://example.com/url', // 個人ページURL
     },
     {
-      name: '大塚晴貴',
-      image: '/~nlplab/member_img/Otsuka.jpg',
+      name: '高井友暉',
+      image: '/~nlplab/member_img/takai.jpg',
       /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
       link: 'https://example.com/url', // 個人ページURL
     },
 　　{
-      name: '岡島史周',
-      image: '/~nlplab/member_img/Okajima.jpg',
-      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
-      link: 'https://fuchiiiii.github.io/', // 個人ページURL
-    },
-    {
-      name: '柴田翔空',
-      image: '/~nlplab/member_img/shibata.jpg',
+      name: '⿃居巧',
+      image: '/~nlplab/member_img/torii.jpg',
       /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
       link: 'https://example.com/url', // 個人ページURL
     },
     {
-      name: '清水綾太',
-      image: '/~nlplab/member_img/shimizu.jpg',
+      name: '⿃巣裕⽃',
+      image: '/~nlplab/member_img/torisu.jpg',
       /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
-      link: 'https://mizuryo108.github.io/', // 個人ページURL
+      link: 'https://example.com/url', // 個人ページURL
+    },
+    {
+      name: '布⽬柊',
+      image: '/~nlplab/member_img/nunome.jpg',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
+      link: 'https://example.com/url', // 個人ページURL
     },
 　  {
-      name: '長谷川騎平',
-      image: '/~nlplab/member_img/kippei.png',
+      name: '三崎颯',
+      image: '/~nlplab/member_img/misaki.png',
       /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
-      link: 'https://sites.google.com/view/kipeo-portfolio', // 個人ページURL
+      link: 'https://example.com/url', // 個人ページURL
     },
 　　{
-      name: '肥田京佳',
-      image: '/~nlplab/member_img/hida.jpg',
+      name: '⼩村将大',
+      image: '/~nlplab/member_img/omura.jpg',
       /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
-      link: 'https://kyoka-hida.github.io', // 個人ページURL
+      link: 'https://example.com/url', // 個人ページURL
     },
     {
-      name: '増井辰真',
-      image: '/~nlplab/member_img/masui.jpg',
+      name: '野村詠介',
+      image: '/~nlplab/member_img/nomura.jpg',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
+      link: 'https://example.com/url', // 個人ページURL
+    },
+    {
+      name: '橋本彩花',
+      image: '/~nlplab/member_img/hashimoto.jpg',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
+      link: 'https://example.com/url', // 個人ページURL
+    },
+    {
+      name: '牧勇吾',
+      image: '/~nlplab/member_img/maki.jpg',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
+      link: 'https://example.com/url', // 個人ページURL
+    },
+    {
+      name: '松⽥隼人',
+      image: '/~nlplab/member_img/matsuda.png',
+      /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
+      link: 'https://example.com/url', // 個人ページURL
+    },
+    {
+      name: '⽔⾕咲良',
+      image: '/~nlplab/member_img/mizutani.jpg',
       /* description: '氏名の下に何かひとこと書きたい人はここに書いてコメントを外してください', */
       link: 'https://example.com/url', // 個人ページURL
     },
