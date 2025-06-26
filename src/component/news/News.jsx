6c30ca72104d7533606cb99e2cd4cd632eb4f041
ryoko tokuhisa <rtokuhisa@aitech.ac.jp>
2025-06-26 12:18:25 +0900

    coverやめる

--- src/component/news/News.jsx
+++ src/component/news/News.jsx
@@ -65,7 +65,7 @@ export default function News() {
         <div className={styles.NewsContainer}>
           {newsData.map((newsItem, index) => (
             <div key={index} className={styles.NewsItem}>
-              <img src={newsItem.image} alt={`news image ${index + 1}`} width={340} height={230} style={{ objectFit: 'cover' }}/>
+              <img src={newsItem.image} alt={`news image ${index + 1}`} width={340} height={230} />
               <div className={styles.NewsText}>
                 <p>
                   <span className ={styles.NewsDate}>{newsItem.date}</span> {/* 各ニュースごとの日付 */}
